5d8a8be4ccdddc7d303a1a1c34a426c8f1928367
Jon <57054770+jjjvvvvv@users.noreply.github.com>
2023-02-23 21:47:56 +0000

    map is fixed and added all buoys with working rss feeds

--- pages/map.py
+++ pages/map.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 df = pd.read_csv('buoylist.csv')
 
+# make new dataframe only when lat and lon are not null
+
+df = df[df['lat'].notna()]
+df = df[df['lon'].notna()]
+
 st.map(data=df, zoom=None, use_container_width=True)
 
 st.subheader('Currently serving these buoys')
